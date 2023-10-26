6dbbb23053fe1efadba4bd9ad42bbb7c73c2c1ae
Matusal3m <matusalemdedeus@gmail.com>
2023-10-25 22:40:01 -0300

    fim do codigo(eu acho)

--- salario.py
+++ salario.py
@@ -1,17 +1,28 @@
-print('\n\n\nColoque o valor do salário de cada funcionário para calcular o valoe total a ser pago para eles. \n****Para encerrar o programa digite "0"****\n')
+print('\n\n\nColoque o valor do salário de cada funcionário para calcular o valor dos abonos. \n****Para encerrar o programa digite "0"****\n')
 salario = []
-salario.insert(0, 1)
+salario.append(None)
 a = 1
-total = 0
+abono = []
+abono.append(None)
+abono_total = 0
+abono_verificar = 0
 while salario[a-1] != 0:
-    salario.insert(a, float(input(f'Digite o salário do profissional {a}:\n')))
-    while salario[a] < 0:
-        salario.insert(a, float(input('Digite o sálario do profissional com um valor válido:\n')))
-    abono = salario[a]*1/5
-    if abono == 0:
-        abono = 0
-    elif abono < 100:
-        abono = 100
-    total += abono
+    salario.append(float(input(f'Digite o salário do profissional {a}:\n')))
+    abono_verificar = salario[a]*1/5
+    if abono_verificar == 0:
+        abono_verificar = 0
+    elif abono_verificar < 100:
+        abono_verificar = 100
+    abono.append(abono_verificar)
+    abono_total += abono_verificar
     a += 1
-print(f'O total a ser pago pela empresa será de R${total}.')
+    
+salario.remove(None)
+salario.remove(0)
+abono.remove(None)
+abono.remove(0)
+
+print(salario, abono)
+for i in range (len(salario)):
+    print(f'O trabalhador {i+1} com salário R${salario[i]} recebeu abono de valor R${abono[i]}.')
+print(f'O total a ser gasto com abonos será de R${abono_total}, houve {a-2} colaboradores e {abono.count(100)} ganharam o valor minimo.')

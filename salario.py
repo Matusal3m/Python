4a9783f3f4c88628407f152ab30e3ec9182f0b4b
Matusal3m <matusalemdedeus@gmail.com>
2023-10-27 19:32:59 -0300

    versão melhorada
    coloquei tratamento de exceções com loop e  o código não aceita mais números negativos. tem tbm umas funções pra organizar melhor

--- salario.py
+++ salario.py
@@ -1,21 +1,35 @@
-print('\n\n\nColoque o valor do salário de cada funcionário para calcular o valor dos abonos. \n****Para encerrar o programa digite "0"****\n')
-salario = []
-salario.append(None)
-a = 1
-abono = []
-abono.append(None)
-abono_total = 0
-abono_verificar = 0
-
-while salario[a-1] != 0:
-    salario.append(float(input(f'Digite o salário do profissional {a} ou encerre o programa:\n' )))
-    abono_verificar = salario[a]*1/5
+def retorna_abono(abono_verificar):
     if abono_verificar == 0:
         abono_verificar = 0
     elif abono_verificar < 100:
         abono_verificar = 100
-    abono.append(abono_verificar)
-    abono_total += abono_verificar
+    return abono_verificar
+
+def verifica_salario(a):
+    while True:
+        try:
+            salario_verificado = float(input(f'Digite o salário do profissional {a} ou digite "0" para encerrar o programa:\n' ))
+            while salario_verificado < 0:
+                print('\nO valor digitado deve ser positivo. Tente Novamente.')
+                salario_verificado = float(input(f'Digite o salário do profissional {a} ou digite "0" para encerrar o programa:\n' ))
+            break
+        except:
+            print('\nO valor inserido foi inválido, digite apenas números positivos. Tente novamente.')
+    return salario_verificado
+
+print('\nColoque o valor do salário de cada funcionário para calcular o valor dos abonos, quantidade de colaboradores, seus salários, respectivos abonos, maior abono e qual o valor do maior abono.\n')
+
+salario = []
+abono = []
+salario.append(None)
+abono.append(None)
+a = 1
+abono_total = 0
+
+while salario[a-1] != 0:
+    salario.append(verifica_salario(a))
+    abono.append(retorna_abono(salario[a]*1/5))
+    abono_total += retorna_abono(salario[a]*1/5)
     a += 1
 
 salario.remove(None)
@@ -23,7 +37,6 @@ salario.remove(0)
 abono.remove(None)
 abono.remove(0)
 
-print(salario, abono)
 for i in range (len(salario)):
     print(f'O trabalhador {i+1} com salário R${salario[i]} recebeu abono de valor R${abono[i]}.')
 print(f'O total a ser gasto com abonos será de R${abono_total}, houve {a-2} colaboradores, {abono.count(100)} ganharam o valor minimo e o maior abono foi de R${max(abono)}.')
